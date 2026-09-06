ae1704d2c74a78445b3218d9fe0c8c4df651e26a
Zhihan Hao <hzh470891@gmail.com>
2026-09-06 16:20:26 +0800

    Delete Lato_Code directory

--- Lato_Code/CODE/setup_lato.m
+++
@@ -1,9 +0,0 @@
-function root = setup_lato()
-%SETUP_LATO Add the user-facing folders of the Lato toolkit to the path.
-root = fileparts(mfilename('fullpath'));
-addpath(root);
-addpath(fullfile(root, 'configs'));
-addpath(fullfile(root, 'gui'));
-addpath(fullfile(root, 'scripts'));
-fprintf('Lato simulation toolkit ready: %s\n', root);
-end
